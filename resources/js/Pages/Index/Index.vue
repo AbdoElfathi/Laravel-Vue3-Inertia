9f727779de106fd1a45bae1a14487bd89f1f4b5a
Abdo <abdessamadelfathi2018@gmail.com>
2023-10-22 16:26:04 +0100

    setup

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Index</h1>
+    <Link href="/hello">Show Page</Link>
+    <div>
+        The message is : {{ message }}
+    </div>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+
+    defineProps({message: String});
+
+</script>
