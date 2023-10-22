f5caf39aa49efb05861f9a770201e3d20cc19972
Abdo <abdessamadelfathi2018@gmail.com>
2023-10-22 18:24:08 +0100

    Latest

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@
 */
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('/hello', [IndexController::class, 'show'])->name('home');
+
+Route::resource('listing', ListingController::class)
+    ->only(['index', 'show']);
