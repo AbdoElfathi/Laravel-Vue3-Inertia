6102a59c569b81f6ca4eb1b4ed297da29405f3a9
Abdo <abdessamadelfathi2018@gmail.com>
2023-10-25 23:01:26 +0100

    latest ui changes

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,25 +1,28 @@
-<template>
-  <Link :href="$route('listing.create')">New Listing</Link>
- 
+<template> 
   <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
       <div class="container mx-auto">
         <nav class="p-4 flex items-center justify-between">
           <div class="text-lg font-medium">
-            <Link :href="route('listing.index')">Listings</Link>
+            <Link href="/listing">Listings</Link>
+          </div>
+          <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+            <Link href="/">Larazillow</Link>
           </div>
           <div>
-            <!-- <Link :href="route('listing.index')"> Larazillow </Link> -->
+            <Link href="listing/create" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
           </div>
-          <div>x</div>
         </nav>
       </div>
   </header>
 
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
+  
+  <main class="container mx-auto p-4">
+    <div v-if="flashSuccess"  class="mb-4 border rounded-md shadow-sm border-green-200 bg-green-50 dark:border-green-800 dark:bg-green-900 p-2">
+      {{ flashSuccess }}
+    </div>
+    <slot>Default</slot>
 
-  <slot>Default</slot>
+  </main>
 
 </template>
 
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('/hello', [IndexController::class, 'show'])->name('home');
 
-// Route::resource('listing', ListingController::class);
-Route::get('/listing', [ListingController::class, 'index'])->name('listing.index');
+Route::resource('listing', ListingController::class);
