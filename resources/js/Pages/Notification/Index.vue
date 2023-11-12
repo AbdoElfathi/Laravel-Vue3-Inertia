abd573ffb81657fae016052043c25a7ed09a4ace
Abdo <abdessamadelfathi2018@gmail.com>
2023-11-12 15:59:26 +0100

    Notifications done

--- resources/js/Pages/Notification/Index.vue
+++ resources/js/Pages/Notification/Index.vue
@@ -1,8 +1,41 @@
 <template>
-    Hello!
+    <h1 class="text-3xl mb-4">This is your notifications</h1>
+
+    <section v-if="notifications.data.length" class="text-gray-700 dark:text-gray-400">
+        <div v-for="notification in notifications.data" :key="notification.id"
+            class="border-b border-gray-200 dark:border-gray-800 py-4 flex justify-between items-center">
+            <div>
+                <span v-if="notification.type === 'App\\Notifications\\OfferMade'">
+                    Offer
+                    <Price :price="notification.data.amount" /> For
+                    <Link :href="route('realtor.listing.show', { listing: notification.data.listing_id })"
+                        class="text-indigo-600 dark:text-indigo-300"> Listing
+                    </Link>
+                    was made
+                </span>
+            </div>
+            <div>
+                <Link :href="route('notification.seen', { notification: notification })" method="PUT" as="button"
+                    v-if="!notification.read_at" class="btn-outline text-xs font-medium uppercase">Make as
+                read</Link>
+            </div>
+        </div>
+    </section>
+    <EmptyState v-else>No Notifications yet</EmptyState>
+
+    <section v-if="notifications.data.length" class="w-full flex justify-center mt-8 mb-8">
+        <Pagination :links="notifications.links" />
+    </section>
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/vue3'
+import Price from '@/Components/Price.vue';
+import EmptyState from '@/Components/UI/EmptyState.vue'
+import Pagination from '@/Components/UI/Pagination.vue'
 
+defineProps({
+    notifications: Object
+})
 </script>
 
